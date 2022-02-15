9b80801349688bee2d5d4e9b0915d944ec754381
Ben <46476247+hexagonatron@users.noreply.github.com>
2022-02-16 00:47:27 +1030

    Reverting back to original.

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, '../.env' )});
 
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/final-project-starter',
